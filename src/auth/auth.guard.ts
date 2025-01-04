8f52da0968e687d07062ede473b2c0c4bd1f0215
Omar <saucyfunnel@gmail.com>
2025-01-03 21:26:30 -0500

    architecture hexagonal || implementando boilerplate con docker mongo nest pt1

--- src/auth/auth.guard.ts
+++
@@ -1,5 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard as PassportAuthGuard } from '@nestjs/passport';
-
-@Injectable()
-export class AuthGuard extends PassportAuthGuard('jwt') { } // Usamos el nombre de la estrategia 'jwt'
